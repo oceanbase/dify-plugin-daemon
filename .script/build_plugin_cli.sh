880dc703453d2ea503bb38d8c1696cfea9dd26ba
toli-ch <anatolii@clearhavencm.com>
2025-09-01 00:21:29 -0500

    Fix OS and architecture detection for Windows (#432)

--- .script/build_plugin_cli.sh
+++ .script/build_plugin_cli.sh
@@ -5,11 +5,27 @@ set -x
 VERSION=${VERSION:-v0.0.1}
 
 # Detect OS and architecture
-OS="${OS:-$(uname -s | tr '[:upper:]' '[:lower:]')}"
-ARCH="${ARCH:-$(uname -m)}"
+UNAME_OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+UNAME_ARCH=$(uname -m)
 
-GOOS=${OS} GOARCH=${ARCH} \
+# Map OS
+case "$UNAME_OS" in
+  linux*)   GOOS=linux ;;
+  darwin*)  GOOS=darwin ;;
+  msys*|mingw*|cygwin*|windows*) GOOS=windows ;;
+  *)        GOOS="$UNAME_OS" ;;
+esac
+
+# Map ARCH
+case "$UNAME_ARCH" in
+  x86_64|amd64) GOARCH=amd64 ;;
+  i386|i686)    GOARCH=386 ;;
+  aarch64)      GOARCH=arm64 ;;
+  armv7l)       GOARCH=arm ;;
+  *)            GOARCH="$UNAME_ARCH" ;;
+esac
+
 go build \
     -ldflags "\
     -X 'main.VersionX=${VERSION}' "\
-    -o dify-plugin-cli-${OS}-${ARCH} ./cmd/commandline
+    -o "dify-plugin-cli-${GOOS}-${GOARCH}" ./cmd/commandline
